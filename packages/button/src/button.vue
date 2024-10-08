bcaab7992755c6ca7db137f58a569faa47a24640
yy727066193 <991755096@qq.com>
2024-10-08 17:33:17 +0800

    add

---
+++ packages/button/src/button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="yangyi-button">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'YangyiButton'
+}
+</script>
+
+<style scoped>
+@import "./button.css";
+</style>
---
+++ packages/input/src/input.vue
@@ -0,0 +1,13 @@
+<template>
+  <input type="text" class="yangyi-input"/>
+</template>
+
+<script>
+export default {
+  name: 'YangyiInput'
+}
+</script>
+
+<style scoped>
+@import "./input.css";
+</style>
